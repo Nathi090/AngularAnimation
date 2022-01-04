220225d88585cd1de95f9b45e0687c0a655db756
Natalia Solano <nathi241298@gmail.com>
2022-01-03 19:13:15 -0600

    i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,94 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { MouseComponent } from './mouse.component'
+import { HomeComponent } from './routes/home.component';
+
+import { BasicComponent } from './routes/basic.component';
+
+import { ContainedComponent } from './routes/contained.component';
+
+import { AdvancedComponent } from './routes/advanced.component';
 
 import { AppComponent } from './app.component';
 
+import { NotFoundComponent } from './routes/notfound.component';
+
+import { AnimboxComponent } from './animbox.component';
+
+const animationRoutes:Routes = [
+
+  {
+
+    path: 'animation-home',
+
+    component: HomeComponent
+
+  },
+
+  {
+
+    path: 'animation-basics',
+
+    component: BasicComponent
+
+  },
+
+  {
+
+    path: 'animation-contained',
+
+    component: ContainedComponent
+
+  },
+
+  {
+
+    path: 'animation-advanced',
+
+    component: AdvancedComponent
+
+  },
+
+  {
+
+    path: '',
+
+    redirectTo: '/animation-home',
+
+    pathMatch: 'full'
+
+  },
+  {
+
+    path: '**',
+    component: NotFoundComponent
+  }
+
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
+    BasicComponent,
+
+    ContainedComponent,
+
+    AdvancedComponent,
+    NotFoundComponent,
+    AnimboxComponent,
+    MouseComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(animationRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
